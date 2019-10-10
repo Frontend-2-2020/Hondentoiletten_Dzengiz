c329f08d46a4b6b336382a2af75246710e84f03e
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-10-10 07:57:54 +0200

    Finished exercise dog toilets. Nearest toilets now having green marker. Other toilets now having pink markers. Hovering over toilet in infoDiv now toggles the corresponding marker orange.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,12 +4,13 @@
 // JS
 import L from "leaflet";
 import axios from "axios";
+import { getDistance } from 'geolib';
 
 // CSS
 import '../styles/index.scss';
 import "leaflet/dist/leaflet.css";
 
-import { dogLocationMarker } from "./markers";
+import { greenMarker, violetMarker, orangeMarker } from "./markers";
 
 
 /**********************
@@ -34,28 +35,126 @@ L.tileLayer('https://api.mapbox.com/styles/v1/alingenomen/cjp9uuri70bd42so6ig01u
 // Functionality
 ////////////////
 
+// Variables
+let toiletCoordinates = [];
+let toilets = [];
+let nearestToilets = [];
+
+/**
+ * Helper functions
+ ***/
+
+// Functionality to toggle the marker orange
+const toggleOrange = index => {
+    nearestToilets[index].setIcon(orangeMarker);
+};
+
+// Functionality to toggle the marker green
+const toggleGreen = index => {
+    nearestToilets[index].setIcon(greenMarker);
+};
+
+// Functionality to locate the user & pan
+const locateAndPan = (latitude, longitude) => {
+    L.marker([latitude, longitude]).addTo(map);
+    map.panTo(new L.LatLng(latitude, longitude));
+};
+
+// Functionality to calculate distance & create objects to work with
+const calculateDistAndGenerateObj = (latitude, longitude) => {
+    toiletCoordinates.forEach((toilet, index) => {
+        const distance = getDistance([latitude, longitude], [toilet[1], toilet[0]]);
+
+        // Generate a new object
+        const newToilet = {
+            distance: distance,
+            name: `locatie ${index}`,
+            coordinates: toilet
+        };
+
+        toilets.push(newToilet);
+    });
+};
+
+// Functionality to sort the toilets
+const sortToilets = () => {
+    toilets.sort((a, b) => {
+        if(a.distance > b.distance) { return 1; }
+        if(a.distance < b.distance) { return -1; }
+        return 0;
+    });
+};
+
+// Functionality to generate all the markers & HTML
+const generateMarkersAndHTML = () => {
+    // Fetch the infoDiv
+    const info = document.getElementById('locations');
+
+    // Put green markers for the nearest 5 locations & put them in the infoDiv
+    for (let i = 0; i < 5; i++) {
+        // Create an infoDiv to fill with data & add eventlisteners for mouseover & mouseleave
+        const infoDiv = document.createElement('div');
+        infoDiv.addEventListener("mouseover", () => toggleOrange(i));
+        infoDiv.addEventListener("mouseleave",() => toggleGreen(i));
+
+        // Fill the div with its contents
+        infoDiv.innerHTML = (
+            `
+                <div>
+                    <h5>${toilets[i].name}</h5>
+                    <strong style="color: gray">Lat: ${toilets[i].coordinates[1]}, Lon: ${toilets[i].coordinates[0]}</strong>
+                    <strong>Distance: ${toilets[i].distance}</strong> 
+                </div>
+            `
+        );
+
+        // Add the div to the target
+        info.appendChild(infoDiv);
+
+        // Add a marker & add it to an array of the nearest toilets
+        const newMarker = L.marker([toilets[i].coordinates[1], toilets[i].coordinates[0]], {icon: greenMarker}).addTo(map);
+        nearestToilets.push(newMarker);
+    }
+
+    // Generate violet markers for the rest
+    for (let i = 5; i < toilets.length; i++) {
+        L.marker([toilets[i].coordinates[1], toilets[i].coordinates[0]], {icon: violetMarker}).addTo(map);
+    }
+};
+
+/**
+ * End of helper functions
+ **/
+
 // Get all the dog toilets in Ghent & create a marker for each one
 axios.get('https://datatank.stad.gent/4/infrastructuur/hondenvoorzieningen.geojson')
     .then(res => {
+
+        // Loop through the coordinates & save them in an array
         res.data.coordinates.forEach(coordinate => {
-            L.marker([coordinate[1], coordinate[0]], {icon:dogLocationMarker}).addTo(map);
+            toiletCoordinates.push(coordinate);
+        });
+
+        // Ask for the user's location
+        navigator.geolocation.getCurrentPosition(position => {
+
+            // Fetch the latitude & longitude out of the coords
+            const { latitude, longitude } = position.coords;
+
+            // Visualise the user's location & pan to its location
+            locateAndPan(latitude, longitude);
+
+            // Loop through all the toilets & calculate the distance from you
+            calculateDistAndGenerateObj(latitude, longitude);
+
+            // // Sort the toilets by distance, ascending
+            sortToilets();
+
+            // Generate markers & html markup
+            generateMarkersAndHTML();
         });
     });
 
-// When the user allows geolocation add a marker at its current location
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-
-        // Fetch the latitude & longitude out of the coords
-        const { latitude, longitude } = position.coords;
-
-        // Add a marker at the user location
-        L.marker([latitude, longitude]).addTo(map);
-
-        // Center map on user location
-        map.panTo(new L.LatLng(latitude, longitude));
-    });
-}
 
 
 
--- src/scripts/markers.js
+++ src/scripts/markers.js
@@ -16,3 +16,31 @@ export const dogLocationMarker = L.icon({
     shadowAnchor: [4, 62],  // the same for the shadow
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
+
+
+export const greenMarker = L.icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+export const violetMarker = L.icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+export const orangeMarker = L.icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-orange.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
